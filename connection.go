18e0f94823913dbcc70bb1fa7a6daef09fc9fa6d
Jason Puglisi <jason@puglisi.im>
2016-09-15 18:05:46 -0400

    Added client structure

--- connection.go
+++ connection.go
@@ -9,103 +9,119 @@ import (
   "net"
 )
 
-// Struct server stores provided server information.
-type server struct {
-  host   string
-  port   uint16
-  secure bool
-  pass   string
+// Server stores provided server information.
+type Server struct {
+  Host   string
+  Port   uint16
+  Secure bool
+  Pass   string
 }
 
-// Struct user stores provided user information, and is not always current.
-type user struct {
-  nick string
-  user string
-  real string
-  mode byte
+// User stores provided user information, and is not always current.
+type User struct {
+  Nick string
+  User string
+  Real string
+  Mode byte
 }
 
-// CreateServer creates and returns a server struct for use in connections.
+// Client stores client/connection information, including original Server and
+// User.
+type Client struct {
+  Active bool
+  Server Server
+  User   User
+  Conn   net.Conn
+}
+
+// CreateServer creates and returns a server for use in connections.
 // (Host)name is an IP address or FQDN. IPv6 addresses must be in [brackets].
 // Secure is true if the specified port uses TLS/SSL.
 // (Pass)word must be an empty string if there is no server password.
-func CreateServer(host string, port uint16, secure bool, pass string) (server,
+func CreateServer(host string, port uint16, secure bool, pass string) (Server,
     error) {
+  // Error if hostname is empty
   if len(host) < 1 {
-    return server{}, errors.New("creating server: no hostname")
+    return Server{}, errors.New("creating server: hostname too short")
   }
 
-  return server{host, port, secure, pass}, nil
+  // Return server
+  return Server{host, port, secure, pass}, nil
 }
 
-// CreateUser creates and returns a user struct for use in connections.
+// CreateUser creates and returns a user for use in connections.
 // Username (uname) and (real)name will default to (nick)name if empty strings.
 // Mode must be 0 for none, 4 for +w, 8 for +i, or 12 for +iw.
-func CreateUser(nick string, uname string, real string, mode byte) (user,
+func CreateUser(nick string, uname string, real string, mode byte) (User,
     error) {
+  // Error if nickname is empty
   if len(nick) < 1 {
-    return user{}, errors.New("creating user: invalid nickname")
+    return User{}, errors.New("creating user: nickname too short")
   }
 
-  if len(uname) < 1 {
-    return user{}, errors.New("creating user: invalid username")
-  }
-
-  if len(real) < 1 {
-    return user{}, errors.New("creating user: invalid realname")
-  }
-
+  // Error if bitmaskk is not 0, 4, 8, or 12
   if mode != 0 && mode != 4 && mode != 8 && mode != 12 {
-    return user{}, errors.New("creating user: invalid mode bitmask")
+    return User{}, errors.New("creating user: mode bitmask invalid")
   }
 
-  return user{nick, uname, real, mode}, nil
+  // Return user
+  return User{nick, uname, real, mode}, nil
 }
 
 // EstablishConnection establishes a connection to the specified IRC server
-// using the specified user information.
-func EstablishConnection(server server, user user) {
-  conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", server.host, server.port))
+// using the specified user information. It sends initial messages as required
+// by the IRC protocol.
+func EstablishConnection(Server Server, User User) (Client, error) {
+  // Attempt connection establishment
+  conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", Server.Host, Server.Port))
 
+  // Return error if one was encountered
   if err != nil {
-    fmt.Printf("Error connecting to server: %s\n", err);
-    return
+    return Client{}, err;
   }
 
-  fmt.Printf("Connected to server %s (%s)\n", server.host, conn.RemoteAddr())
+  // Create client with connection details
+  Client := Client{true, Server, User, conn}
 
-  go readLoop(conn)
+  // Start read loop to get data from server
+  go readLoop(Client)
 
-  sendRawf(conn, "NICK %s", user.nick)
-  sendRawf(conn, "USER %s %d * :%s", user.user, user.mode, user.real)
+  // Send initial messages to server
+  sendRawf(Client, "NICK %s", User.Nick)
+  sendRawf(Client, "USER %s %d * :%s", User.User, User.Mode, User.Real)
 
-  for {}
+  // Return client
+  return Client, nil;
 }
 
-func Err() {
 
-}
+// readLoop is a goroutine used to read data from the server a client is
+// connected to.
+func readLoop(Client Client) {
+  // Create reader to get data from server
+  reader := bufio.NewReader(Client.Conn)
 
-func readLoop(conn net.Conn) {
-  reader := bufio.NewReader(conn)
-
-  fmt.Println("Started read loop")
-
-  for {
+  // Loop through reads until client is stopped
+  for Client.Active {
     msg, err := reader.ReadString('\n')
 
+    // Output error if encountered
     if err != nil {
-      fmt.Printf("%s\n", err);
+      fmt.Printf("[ERROR] %s\n", err);
     }
 
+    // Output data
     fmt.Printf(msg)
   }
 }
 
-func sendRaw(conn net.Conn, msg string) {
-  fmt.Fprint(conn, msg + "\r\n");
+// sendRawf formats a string to create a raw IRC message that is sent to the
+// client's server.
+func sendRawf(Client Client, format string, a ...interface{}) {
+  fmt.Fprintf(Client.Conn, format + "\r\n", a...);
 }
 
-func sendRawf(conn net.Conn, format string, a ...interface{}) {
-  fmt.Fprintf(conn, format + "\r\n", a...);
+// sendRaw sends a raw IRC message to the client's server.
+func sendRaw(Client Client, msg string) {
+  fmt.Fprint(Client.Conn, msg + "\r\n");
 }
--- connection_test.go
+++ connection_test.go
@@ -14,24 +14,66 @@ func TestCreateServer(t *testing.T) {
     {"localhost", 1, true, "Password1!", ""},
     {"10.0.0.1", 65534, false, "", ""},
     {"::1", 65535, false, "", ""},
-    {"", 0, false, "", "creating server: no hostname"},
+    {"", 0, false, "", "creating server: hostname too short"},
   }
 
   for _, c := range cases {
     _, err := CreateServer(c.host, c.port, c.secure, c.pass);
-    if (err == nil) && len(c.err) > 0 {
+
+    if err == nil && len(c.err) > 0 {
       t.Errorf("CreateServer(\"%s\", %d, %t, \"%s\") did not produce an " +
         "error, expected \"%s\"", c.host, c.port, c.secure, c.pass, c.err)
     }
 
-    if (err != nil) && len(c.err) < 1 {
+    if err != nil && len(c.err) < 1 {
       t.Errorf("CreateServer(\"%s\", %d, %t, \"%s\") produced an error, " +
         "expected no error", c.host, c.port, c.secure, c.pass)
     }
 
-    if (err != nil) && (err.Error() != c.err) {
+    if err != nil && err.Error() != c.err {
       t.Errorf("CreateServer(\"%s\", %d, %t, \"%s\") produced an incorrect " +
-        "error \"%s\", expected \"%s\"", c.host, c.port, c.secure, c.pass, err, c.err)
+        "error \"%s\", expected \"%s\"", c.host, c.port, c.secure, c.pass, err,
+        c.err)
+    }
+  }
+}
+
+func TestCreateUser(t *testing.T) {
+  cases := []struct{
+    nick string
+    user string
+    real string
+    mode byte
+    err string
+  }{
+    {"Inami", "inami", "Mahiru Inami", 0, ""},
+    {"I", "i", "I", 4, ""},
+    {"Inami", "inami", "Mahiru Inami", 8, ""},
+    {"Inami", "inami", "Mahiru Inami", 12, ""},
+    {"", "inami", "Mahiru Inami", 0, "creating user: nickname too short"},
+    {"Inami", "inami", "Mahiru Inami", 1, "creating user: mode bitmask " +
+      "invalid"},
+    {"Inami", "inami", "Mahiru Inami", 13, "creating user: mode bitmask " +
+      "invalid"},
+  }
+
+  for _, c := range cases {
+    _, err := CreateUser(c.nick, c.user, c.real, c.mode);
+
+    if err == nil && len(c.err) > 0 {
+      t.Errorf("CreateUser(\"%s\", \"%s\", \"%s\", %d) did not produce an " +
+        "error, expected \"%s\"", c.nick, c.user, c.real, c.mode, c.err)
+    }
+
+    if err != nil && len(c.err) < 1 {
+      t.Errorf("CreateUser(\"%s\", \"%s\", \"%s\", %d) produced an error, " +
+        "expected no error", c.nick, c.user, c.real, c.mode)
+    }
+
+    if err != nil && err.Error() != c.err {
+      t.Errorf("CreateUser(\"%s\", \"%s\", \"%s\", %d) produced an incorrect " +
+        "error \"%s\", expected \"%s\"", c.nick, c.user, c.real, c.mode, err,
+        c.err)
     }
   }
 }
