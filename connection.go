1a599193886c97954a88742dec84ddd88ddb9bff
Jason Puglisi <jason@puglisi.im>
2016-09-15 18:33:52 -0400

    Clean up comments and make more struct variables private

--- connection.go
+++ connection.go
@@ -9,29 +9,30 @@ import (
   "net"
 )
 
-// Server stores provided server information.
+// Server stores server connection details.
 type Server struct {
-  Host   string
-  Port   uint16
-  Secure bool
-  Pass   string
+  host   string
+  port   uint16
+  secure bool
+  pass   string
 }
 
-// User stores provided user information, and is not always current.
+// User stores initial connection user details.
 type User struct {
-  Nick string
-  User string
-  Real string
-  Mode byte
+  nick string
+  user string
+  real string
+  mode byte
 }
 
-// Client stores client/connection information, including original Server and
-// User.
+// Client stores initial server/user details, client status, client channels,
+// and active client connection.
 type Client struct {
-  Active bool
-  Server Server
-  User   User
-  Conn   net.Conn
+  Active  bool
+  server  Server
+  user    User
+  conn    net.Conn
+  errCh   chan error
 }
 
 // CreateServer creates and returns a server for use in connections.
@@ -40,12 +41,11 @@ type Client struct {
 // (Pass)word must be an empty string if there is no server password.
 func CreateServer(host string, port uint16, secure bool, pass string) (Server,
     error) {
-  // Error if hostname is empty
+  // Error if hostname is empty.
   if len(host) < 1 {
     return Server{}, errors.New("creating server: hostname too short")
   }
 
-  // Return server
   return Server{host, port, secure, pass}, nil
 }
 
@@ -54,74 +54,65 @@ func CreateServer(host string, port uint16, secure bool, pass string) (Server,
 // Mode must be 0 for none, 4 for +w, 8 for +i, or 12 for +iw.
 func CreateUser(nick string, uname string, real string, mode byte) (User,
     error) {
-  // Error if nickname is empty
+  // Error if nickname is empty.
   if len(nick) < 1 {
     return User{}, errors.New("creating user: nickname too short")
   }
 
-  // Error if bitmaskk is not 0, 4, 8, or 12
+  // Error if bitmaskk is not 0, 4, 8, or 12.
   if mode != 0 && mode != 4 && mode != 8 && mode != 12 {
     return User{}, errors.New("creating user: mode bitmask invalid")
   }
 
-  // Return user
   return User{nick, uname, real, mode}, nil
 }
 
 // EstablishConnection establishes a connection to the specified IRC server
 // using the specified user information. It sends initial messages as required
 // by the IRC protocol.
-func EstablishConnection(Server Server, User User) (Client, error) {
-  // Attempt connection establishment
-  conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", Server.Host, Server.Port))
-
-  // Return error if one was encountered
+func EstablishConnection(server Server, user User) (Client, error) {
+  // Attempt connection establishment.
+  conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", server.host, server.port))
   if err != nil {
     return Client{}, err;
   }
 
-  // Create client with connection details
-  Client := Client{true, Server, User, conn}
+  // Create client with with server, user, and connection. Start reading from
+  // server.
+  client := Client{true, server, user, conn, make(chan error)}
+  go readLoop(client)
 
-  // Start read loop to get data from server
-  go readLoop(Client)
+  // Send required user registration messages to server.
+  sendRawf(client, "NICK %s", user.nick)
+  sendRawf(client, "USER %s %d * :%s", user.user, user.mode, user.real)
 
-  // Send initial messages to server
-  sendRawf(Client, "NICK %s", User.Nick)
-  sendRawf(Client, "USER %s %d * :%s", User.User, User.Mode, User.Real)
-
-  // Return client
-  return Client, nil;
+  return client, nil;
 }
 
 
 // readLoop is a goroutine used to read data from the server a client is
 // connected to.
-func readLoop(Client Client) {
-  // Create reader to get data from server
-  reader := bufio.NewReader(Client.Conn)
-
-  // Loop through reads until client is stopped
-  for Client.Active {
+func readLoop(client Client) {
+  // Create reader for server data, and loop reads until client is stopped.
+  reader := bufio.NewReader(client.conn)
+  for client.Active {
     msg, err := reader.ReadString('\n')
-
-    // Output error if encountered
     if err != nil {
-      fmt.Printf("[ERROR] %s\n", err);
+      client.errCh <- err
     }
 
-    // Output data
     fmt.Printf(msg)
   }
 }
 
 // sendRawf formats a string to create a raw IRC message that is sent to the
-// client's server.
-func sendRawf(Client Client, format string, a ...interface{}) {
-  fmt.Fprintf(Client.Conn, format + "\r\n", a...);
+// client's server. It appends necessary line endings.
+func sendRawf(client Client, format string, a ...interface{}) {
+  fmt.Fprintf(client.conn, format + "\r\n", a...);
 }
 
-// sendRaw sends a raw IRC message to the client's server.
-func sendRaw(Client Client, msg string) {
-  fmt.Fprint(Client.Conn, msg + "\r\n");
+// sendRaw sends a raw IRC message to the client's server. It appends necessary
+// line endings.
+func sendRaw(client Client, msg string) {
+  fmt.Fprint(client.conn, msg + "\r\n");
 }
